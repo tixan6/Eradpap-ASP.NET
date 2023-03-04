237110a3fde4b3bae071869c502a46037cad2488
Evgen <Evgen@PIUIOP>
2023-03-04 08:54:38 +0300

    Добавьте файлы проекта.

---
+++ Eradpap/AdminLogin.aspx
@@ -0,0 +1,45 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdminLogin.aspx.cs" Inherits="Eradpap.WebForm1" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<link href="www/adminLogin.css" rel="stylesheet" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <title>Панель администрации</title>
+</head>
+<body>
+
+    <form id="form1" runat="server">
+        <div class="login">
+
+            <div class="label">
+                <h1>Eradpap</h1>
+                <p>Администрирование товара</p>
+            </div>
+
+
+            <div class="baground-input">
+
+                <div class="CenterItems">
+                    <asp:TextBox runat="server" ID="LoginUser" placeholder="Логин" CssClass="input"/> <br />
+                    <asp:TextBox runat="server" ID="PasswordUser" placeholder="Пароль" CssClass="input"/> <br />
+
+                    <asp:Button Text="Войти" runat="server" CssClass="buttonSigin" OnClick="SetUpConnection"/>
+                    <asp:Label Text="text" ID="test" runat="server" />
+                </div>
+                
+
+            </div>
+
+
+
+            
+        </div>
+        
+    </form>
+
+</body>
+
+
+</html>
---
+++ Eradpap/AdminLogin.aspx.cs
@@ -0,0 +1,57 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Common;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Eradpap
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+
+
+
+        }
+
+        public void SetUpConnection(object sender, EventArgs e) 
+        {
+            //Выучить нахой!!!
+
+            String connectionString = "Server=localhost;Port=5432;Username=postgres;Password=159753;Database=Eradpap;";
+            NpgsqlConnection npgSqlConnection = new NpgsqlConnection(connectionString);
+
+            npgSqlConnection.Open();
+ 
+            NpgsqlCommand npgSqlCommand = new NpgsqlCommand("SELECT * FROM publics.\"dtAdmin\";", npgSqlConnection);
+
+            NpgsqlDataReader npgSqlDataReader = npgSqlCommand.ExecuteReader();
+            if (npgSqlDataReader.HasRows)
+            {
+                while (npgSqlDataReader.Read()) // построчно считываем данные
+                {
+                    object pass = npgSqlDataReader.GetValue(0);
+                    object log = npgSqlDataReader.GetValue(1);
+
+
+                    test.Text = pass.ToString();
+                }
+
+
+            }
+            else
+                test.Text = "Няма нихуя";
+           
+        }
+
+
+    }
+}
